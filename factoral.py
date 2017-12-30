533b6e54392b8326575b4f444243d99a8f27ba56
adam-hamilton <ahamilton7@live.com>
2017-12-30 11:06:40 -0600

    updated factoral example

--- factoral.py
+++ factoral.py
@@ -1,8 +1,39 @@
 #!/usr/bin/python
 
-def fact(x):
+def fact(num):
+    """
+    Args:
+        factoral input number
+    Returns:
+        computed factoral
+    """
+
     result = 1
-    for i in range(2, x + 1):
+    for i in range(2, num + 1):
         result *= i
     return result
-print(fact(4))
+
+def main():
+    """
+    Args:
+        None - requires user input of factoral values
+    Returns:
+        Factoral result
+    """
+
+    while True:
+        number = input("Enter a number: ")
+        try:
+            val = int(number)
+            if val < 0:  # if not a positive int print message and ask for input again
+                print("Sorry, input must be a positive integer, try again")
+                continue
+            break
+        except ValueError:
+            print("That's not an int!")
+
+    result = fact(number)
+    print(result)
+
+if __name__ == '__main__':
+    main()
